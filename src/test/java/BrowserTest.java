b1c73abb8da1d95c6d41e0b06371f815df62f992
fokevolt <fokevolt@gmail.com>
2022-04-11 19:04:11 +0300

    Initialized repository and added sample JAVA+Gradle+JUnit+Selenium project

---
+++ src/test/java/BrowserTest.java
@@ -0,0 +1,33 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BrowserTest {
+    WebDriver driver;
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {ChromeDriver.class, EdgeDriver.class})
+    void test(Class<? extends WebDriver> webDriverClass) {
+        // Driver management and WebDriver instantiation
+        driver = WebDriverManager.getInstance(webDriverClass).create();
+
+        // Exercise
+        driver.get("https://rozetka.com.ua/");
+        String title = driver.getTitle();
+
+        // Verify
+        assertTrue(title.contains("Интернет-магазин ROZETKA"));
+    }
+
+}
